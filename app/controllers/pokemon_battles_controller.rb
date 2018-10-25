9f55d5c92a98b05e166c9db08a67c2d967d0aa44
Yuli <yuli23amelia@gmail.com>
2018-10-25 16:51:52 +0700

    refactor

--- app/controllers/pokemon_battles_controller.rb
+++ app/controllers/pokemon_battles_controller.rb
@@ -1,5 +1,6 @@
 class PokemonBattlesController < ApplicationController
   before_action :set_pokemon_battle, only: [:show, :edit, :update, :destroy, :attack, :surrender]
+  before_action :set_attacker_defender, only: [:attack, :surrender]
 
   def index
     @pokemon_battle = PokemonBattle.reorder("created_at DESC").all.paginate(:page => params[:page], :per_page => 5)
@@ -44,189 +45,21 @@ class PokemonBattlesController < ApplicationController
   end
 
   def attack
-    pokemon1 = Pokemon.find(@pokemon_battle.pokemon1_id)
-    pokemon1_current_experience = pokemon1.current_experience
-    pokemon2 = Pokemon.find(@pokemon_battle.pokemon2_id)
-    pokemon2_current_experience = pokemon2.current_experience
-    skill_id = params[:skill_id]
-    current_turn = @pokemon_battle.current_turn
-    bonus = PokemonBattleCalculator.calculate_level_up_extra_state
-
-    if current_turn % 2 == 0
-
-      pokemon_skill = PokemonSkill.where("skill_id = ? AND pokemon_id = ?", skill_id, pokemon2).first
-      damage = PokemonBattleCalculator.calculate_damage(pokemon2, pokemon1, skill_id)
-      current_hp = pokemon1.current_health_point - damage
-      pokemon_current_hp = current_hp < 0 ? 0 : current_hp
-      current_pp = pokemon_skill.current_pp - 1
-      current_turn = @pokemon_battle.current_turn + 1
-      experience_gain = PokemonBattleCalculator.calculate_experience(pokemon1.level)
-
-      if pokemon_skill.current_pp == 0
-          flash[:error] = "#{@pokemon_battle.pokemon2.name} sudah tidak punya kekuatan!"
-          redirect_to @pokemon_battle
-      else
-        if pokemon_current_hp == 0
-           health_point = pokemon2.max_health_point + bonus.health_point
-           attack_point = pokemon2.attack + bonus.attack_poin
-           defence_point = pokemon2.defence + bonus.defence_point
-           speed_point = pokemon2.speed + bonus.speed_point
-           total_exprience_pokemon_winner = pokemon2_current_experience + experience_gain
-           @pokemon_battle.update(pokemon_winner_id: pokemon2.id)
-           @pokemon_battle.update(pokemon_loser_id: pokemon1.id)
-           @pokemon_battle.update(pokemon_winner_id: pokemon2.id,
-                                  pokemon_loser_id: pokemon1.id,
-                                  state: "finish",
-                                  experience_gain: experience_gain)
-           pokemon2.update(max_health_point: health_point,
-                           attack: attack_point,
-                           defence: defence_point,
-                           speed: speed_point,
-                           current_experience: total_exprience_pokemon_winner)
-
-          #level_up?
-         level_pokemon_winner = pokemon1.level
-         level_up = PokemonBattleCalculator.level_up?(level_pokemon_winner, total_exprience_pokemon_winner)
-
-         if level_up == true
-          experience_turn = total_exprience_pokemon_winner / 100
-          level = Math.log2(experience_turn).to_i + 1
-          pokemon2.update(level: level)
-         end
-
-        end
-        pokemon1.update(current_health_point: pokemon_current_hp)
-        pokemon_skill.update(current_pp: current_pp)
-        @pokemon_battle.update(current_turn: current_turn)
-
-        redirect_to @pokemon_battle, notice: "GOOD JOB #{@pokemon_battle.pokemon2.name}"
-      end
-
+    if @current_turn % 2 == 0
+      attack_action(skill_id: @skill_id, attacker: @pokemon2, defender: @pokemon1)
     else
-      pokemon_skill = PokemonSkill.where("skill_id = ? AND pokemon_id = ?", skill_id, pokemon1).first
-      damage = PokemonBattleCalculator.calculate_damage(pokemon1, pokemon2, skill_id)
-      current_hp = pokemon2.current_health_point - damage
-      pokemon_current_hp = current_hp < 0 ? 0 : current_hp
-      current_pp = pokemon_skill.current_pp - 1
-      current_turn = @pokemon_battle.current_turn + 1
-      experience_gain = PokemonBattleCalculator.calculate_experience(pokemon2.level)
-
-      if pokemon_skill.current_pp == 0
-        flash[:error] = "#{@pokemon_battle.pokemon1.name} sudah tidak punya kekuatan!"
-        redirect_to @pokemon_battle
-      else
-
-       if pokemon_current_hp == 0
-          health_point = pokemon1.max_health_point + bonus.health_point
-          attack_point = pokemon1.attack + bonus.attack_point
-          defence_point = pokemon1.defence + bonus.defence_point
-          speed_point = pokemon1.speed + bonus.speed_point
-          total_exprience_pokemon_winner = pokemon1_current_experience + experience_gain
-          @pokemon_battle.update(pokemon_winner_id: pokemon1.id,
-                                 pokemon_loser_id: pokemon2.id,
-                                 state: "finish",
-                                 experience_gain: experience_gain)
-          pokemon1.update(max_health_point: health_point,
-                          attack: attack_point,
-                          defence: defence_point,
-                          speed: speed_point,
-                          current_experience: total_exprience_pokemon_winner)
-
-
-          #level_up?
-         level_pokemon_winner = pokemon1.level
-         level_up = PokemonBattleCalculator.level_up?(level_pokemon_winner, total_exprience_pokemon_winner)
-
-         if level_up == true
-          experience_turn = total_exprience_pokemon_winner / 100
-          level = Math.log2(experience_turn).to_i + 1
-          pokemon1.update(level: level)
-         end
-
-         flash[:success] = "#{@pokemon_battle.pokemon1.name} Menang"
-       end
-        pokemon2.update(current_health_point: pokemon_current_hp)
-        pokemon_skill.update(current_pp: current_pp)
-        @pokemon_battle.update(current_turn: current_turn)
-
-        flash[:success] = "GOOD JOB #{@pokemon_battle.pokemon1.name}"
-        redirect_to @pokemon_battle
-      end
+      attack_action(skill_id: @skill_id, attacker: @pokemon1, defender: @pokemon2)
     end
   end
 
   def surrender
-    pokemon1 = Pokemon.find(@pokemon_battle.pokemon1_id)
-    pokemon1_current_experience = pokemon1.current_experience
-    pokemon2 = Pokemon.find(@pokemon_battle.pokemon2_id)
-    pokemon2_current_experience = pokemon2.current_experience
-    current_turn = @pokemon_battle.current_turn
-    experience_gain = PokemonBattleCalculator.calculate_experience(pokemon1.level)
-    bonus = PokemonBattleCalculator.calculate_level_up_extra_state
 
+    if @current_turn % 2 == 0
 
-    if current_turn % 2 == 0
-
-      health_point = pokemon1.max_health_point + bonus.health_point
-      attack_point = pokemon1.attack + bonus.attack_point
-      defence_point = pokemon1.defence + bonus.defence_point
-      speed_point = pokemon1.speed + bonus.speed_point
-      total_exprience_pokemon_winner = pokemon1_current_experience + experience_gain
-      @pokemon_battle.update(pokemon_winner_id: pokemon1.id,
-                             pokemon_loser_id: pokemon2.id,
-                             state: "finish",
-                             experience_gain: experience_gain)
-      pokemon1.update(max_health_point: health_point,
-                      attack: attack_point,
-                      defence: defence_point,
-                      speed: speed_point,
-                      current_experience: total_exprience_pokemon_winner)
-
-
-      #level_up?
-      level_pokemon_winner = pokemon1.level
-      level_up = PokemonBattleCalculator.level_up?(level_pokemon_winner, total_exprience_pokemon_winner)
-
-      if level_up == true
-       experience_turn = total_exprience_pokemon_winner / 100
-       level = Math.log2(experience_turn).to_i + 1
-       pokemon1.update(level: level)
-      end
-
-      flash[:success] = "#{@pokemon_battle.pokemon1.name} Menang"
-      redirect_to @pokemon_battle
+      game_finish(winner: @pokemon1, loser: @pokemon2)
     else
 
-      health_point = pokemon2.max_health_point + bonus.health_point
-      attack_point = pokemon2.attack + bonus.attack_poin
-      defence_point = pokemon2.defence + bonus.defence_point
-      speed_point = pokemon2.speed + bonus.speed_point
-      total_exprience_pokemon_winner = pokemon2_current_experience + experience_gain
-      @pokemon_battle.update(pokemon_winner_id: pokemon2.id)
-      @pokemon_battle.update(pokemon_loser_id: pokemon1.id)
-      @pokemon_battle.update(pokemon_winner_id: pokemon2.id,
-                             pokemon_loser_id: pokemon1.id,
-                             state: "finish",
-                             experience_gain: experience_gain)
-      pokemon2.update(max_health_point: health_point,
-                      attack: attack_point,
-                      defence: defence_point,
-                      speed: speed_point,
-                      current_experience: total_exprience_pokemon_winner)
-
-
-      #level_up?
-      level_pokemon_winner = pokemon2.level
-      level_up = PokemonBattleCalculator.level_up?(level_pokemon_winner, total_exprience_pokemon_winner)
-
-      if level_up == true
-       experience_turn = total_exprience_pokemon_winner / 100
-       level = Math.log2(experience_turn).to_i + 1
-       pokemon2.update(level: level)
-      end
-
-      flash[:success] = "#{@pokemon_battle.pokemon2.name} Menang"
-      redirect_to @pokemon_battle
+      game_finish(winner: @pokemon2, loser: @pokemon1)
     end
   end
 
@@ -240,4 +73,68 @@ class PokemonBattlesController < ApplicationController
     params.require(:pokemon_battle).permit(:pokemon1_id, :pokemon2_id)
   end
 
+  def set_attacker_defender
+    @pokemon1 = Pokemon.find(@pokemon_battle.pokemon1_id)
+    @pokemon2 = Pokemon.find(@pokemon_battle.pokemon2_id)
+    @current_turn = @pokemon_battle.current_turn
+    @skill_id = params[:skill_id]
+  end
+
+  def bonus(winner:)
+    bonus = PokemonBattleCalculator.calculate_level_up_extra_state
+    health_point = winner.max_health_point + bonus.health_point
+    attack_point = winner.attack + bonus.attack_point
+    defence_point = winner.defence + bonus.defence_point
+    speed_point = winner.speed + bonus.speed_point
+
+    winner.update(max_health_point: health_point,
+                    attack: attack_point,
+                    defence: defence_point,
+                    speed: speed_point)
+  end
+
+  def game_finish(winner:, loser:)
+     experience_gain = PokemonBattleCalculator.calculate_experience(loser.level)
+     winner_current_experience = winner.current_experience
+     total_exprience_pokemon_winner = winner_current_experience + experience_gain
+     @pokemon_battle.update(pokemon_winner_id: winner.id,
+                            pokemon_loser_id: loser.id,
+                            state: "finish",
+                            experience_gain: experience_gain)
+     winner.update(current_experience: total_exprience_pokemon_winner)
+      #level_up?
+     level_pokemon_winner = winner.level
+     level_up = PokemonBattleCalculator.level_up?(level_pokemon_winner, total_exprience_pokemon_winner)
+     if level_up == true
+        experience_turn = total_exprience_pokemon_winner / 100
+        bonus(winner: winner)
+        level = Math.log2(experience_turn).to_i + 1
+        winner.update(level: level)
+     end
+     flash[:success] = "Selamat #{winner.name} memenangkan pertandingan ini!"
+     redirect_to @pokemon_battle
+  end
+
+  def attack_action(skill_id:, attacker:, defender:)
+    pokemon_skill = PokemonSkill.where("skill_id = ? AND pokemon_id = ?", skill_id, attacker).first
+    damage = PokemonBattleCalculator.calculate_damage(attacker, defender, skill_id)
+    current_hp = defender.current_health_point - damage
+    pokemon_current_hp = current_hp < 0 ? 0 : current_hp
+    current_pp = pokemon_skill.current_pp - 1
+    current_turn = @pokemon_battle.current_turn + 1
+
+    if pokemon_skill.current_pp == 0
+        flash[:error] = "#{attacker.name} sudah tidak punya kekuatan!"
+        redirect_to @pokemon_battle
+    else
+      if pokemon_current_hp == 0
+         game_finish(winner:attacker, loser:defender)
+      end
+      defender.update(current_health_point: pokemon_current_hp)
+      pokemon_skill.update(current_pp: current_pp)
+      @pokemon_battle.update(current_turn: current_turn)
+      flash[:success] = "GOOD JOB #{attacker.name}"
+      redirect_to @pokemon_battle
+    end
+  end
 end
